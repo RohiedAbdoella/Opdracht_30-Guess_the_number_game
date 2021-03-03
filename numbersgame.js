2e2bcbb1321238483a81ce7943403ef5242d1035
Rohied Abdoella <74419576+RohiedAbdoella@users.noreply.github.com>
2021-03-03 21:12:32 +0100

    Add files via upload

---
+++ numbersgame.js
@@ -0,0 +1,16 @@
+let greet = prompt("Welkom! Wat is je naam?");
+
+let hey = alert("Hey " + greet);
+
+let getNumber = prompt("Voer een nummer in van 0 tot 25 om te beginnen met raden");
+
+let winChance = math.random (0, 25) {
+      if (winChance === getNumber) {
+       alert("Gefeliciteerd, je hebt gewonnen")
+      } else  alert("Dat is niet correct");
+}
+
+
+let bye = alert("Dag " + greet + ". Tot de volgende keer");
+
+//bonus
